abef0b2370d4ff1b81afcb5f09b8325cb20edab6
PapstJL4U <jedi-papstjl@gmx.de>
2022-05-17 15:21:39 +0200

    reader

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,11 +1,12 @@
 ﻿//initial CLI
 using System;
+using foodLib;
 namespace CLI
 {
     class CLI
     {
         public static void Main(string[] args){
-            
+            foodLib.foodLib.FileReader();
             Console.WriteLine("Done! Thank you!");
         }
     }
--- foodLib/foodlib.cs
+++ foodLib/foodlib.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 namespace foodLib;
 public static class foodLib
 {   
+    private static string path = "food.tsv";
     public static Dictionary<string, string[]> Rezepte = new Dictionary<string, string[]>();
     public static void FileReader()
     {
-        
+        using(StreamReader SR = File.OpenText(path))
+        {
+            string line;
+            while ((line = SR.ReadLine()) != null)
+            {   
+                string[] subs = line.Split("\t");
+                Console.WriteLine(subs[0]);
+            }
+        }
     }
 }
