69e0811838fc9eef3965f8a5d919ce93c891a9b4
PapstJL4U <jedi-papstjl@gmx.de>
2022-05-17 16:27:09 +0200

    rezept finder: name, zutat

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -7,6 +7,10 @@ namespace CLI
     {
         public static void Main(string[] args){
             foodLib.foodLib.FileReader();
+            var one = foodLib.foodLib.RezeptName("joe");
+            Console.WriteLine($"{one.Count}");
+            var two = foodLib.foodLib.RezeptZutat("käse");
+            Console.WriteLine($"{two.Count}");
             Console.WriteLine("Done! Thank you!");
         }
     }
--- foodLib/foodlib.cs
+++ foodLib/foodlib.cs
@@ -5,7 +5,13 @@ namespace foodLib;
 public static class foodLib
 {   
     private static string path = "food.tsv";
-    public static Dictionary<string, string[]> Rezepte = new Dictionary<string, string[]>();
+    private static Dictionary<string, List<string>> _Rezepte = new Dictionary<string, List<string>>();
+
+    public static Dictionary<string, List<string>> Rezepte()
+    {
+        return _Rezepte;
+    }
+
     public static void FileReader()
     {
         using(StreamReader SR = File.OpenText(path))
@@ -14,8 +20,37 @@ public static class foodLib
             while ((line = SR.ReadLine()) != null)
             {   
                 string[] subs = line.Split("\t");
-                Console.WriteLine(subs[0]);
+                string key = subs[0];
+                List<string> value = subs.ToList<string>();
+                value.Remove(key);
+
+                _Rezepte.Add(key, value);
             }
         }
+    }
+
+    public static List<string> RezeptName(string Name){
+        List<string> Liste = new List<string>();
+        foreach(var entry in _Rezepte.Keys)
+        {
+            if(entry.Contains(Name))
+            {
+                Liste.Add(entry);
+            }
+        }
+        return Liste;
+    }
+
+    public static List<string> RezeptZutat(string Zutat){
+        List<string> Liste = new List<string>();
+        foreach(var entry in _Rezepte.Keys)
+        {
+            List<string> Zutaten = _Rezepte[entry];
+            if(Zutaten.Contains(Zutat))
+            {
+                Liste.Add(entry);
+            }
+        }
+        return Liste;
     }
 }
