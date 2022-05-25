3e5ce8c32d3647bbf3122986b63f2eb610eba916
PapstJL4U <PapstJL4U@users.noreply.github.com>
2022-05-25 08:58:52 +0200

    mainwindow logic added for single search

--- FoodGUI/MainWindow.xaml.cs
+++ FoodGUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Food=foodLib.foodLib;
 
 namespace FoodGUI
 {
@@ -23,6 +24,7 @@ namespace FoodGUI
         public MainWindow()
         {
             InitializeComponent();
+            Food.FileReader();
         }
 
         private void ButtonSuche_Click(object sender, RoutedEventArgs e)
@@ -30,10 +32,51 @@ namespace FoodGUI
             if (!string.IsNullOrWhiteSpace(txtSuche.Text))
             {   
                 string suche = $"{txtSuche.Text}";
+                string[] input = suche.Split(",");
+                for(int i=0; i<input.Length; i++)
+                {//no whitespace!
+                    input[i] = input[i].TrimStart().TrimEnd();
+                }
+                if(input.Count()==1)
+                {
+                    List<string> antwort = Click_on_Single(input[0]);
+                }
+
+
+
+
+
+                //AUSGABE ENDE
                 Ausgabe.Items.Add($"==Suche für: {suche}==");
                 Ausgabe.Items.Add(txtSuche.Text);
-                txtSuche.Clear();
+                //txtSuche.Clear();
+            }
+            else
+            {   
+                string suche ="ZUFALL";
+                var rez = foodLib.foodLib.RNGRezept();
+                Ausgabe.Items.Add($"==Suche für: {suche}==");
+                Ausgabe.Items.Add(rez);
             }
         }
+        //Verarbeitet die Eingabe eine einzigen suchwortes
+        private List<string> Click_on_Single(string ein_wort_suche)
+        {
+            List<string> rezept_name = Food.RezeptName(ein_wort_suche);
+            List<string> rezept_zutat = Food.RezeptZutat(ein_wort_suche);
+            var antwort = (rezept_zutat.Union(rezept_name)).ToList();
+
+            return antwort;
+        }
+
+        private void Mausgabe(string eingabe, List<string> antwort)
+        {
+            Ausgabe.Items.Add($"==Suche für: {eingabe}==");
+            foreach(string s in antwort)
+            {
+                Ausgabe.Items.Add(Food.Rezepte()[s]);
+            }
+        }
+
     }
 }
